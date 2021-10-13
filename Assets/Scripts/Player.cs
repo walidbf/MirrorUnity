40bcdd09d6ac6372bb09055eed41344f4005e276
walidbf <walid.benftima@esprit.tn>
2021-10-13 17:54:17 +0100

    First exemple Using cubes

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizental = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizental * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+    private void Update()
+    {
+        HandleMovement();
+    }
+}
